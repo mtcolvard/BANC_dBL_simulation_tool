afabcc54a6b19755b45b6bf3018e90e429f0713f
mtcolvard <matthew.colvard@gmail.com>
2021-07-17 22:00:08 +0200

    transfered loudThumpingFootfalls

---
+++ banc_modules/audio/loudThumpingFootfalls.js
@@ -0,0 +1,10 @@
+<section class="distantFreeway">
+
+  <audio src="https://freesound.org/people/wlabarron/sounds/451240/" crossorigin="anonymous" ></audio>
+
+<!-- 			type="audio/mpeg" -->
+
+  <button data-playing="false" class="tape-controls-play" role="switch" aria-checked="false">
+    <span>Play/Pause</span>
+  </button>
+</section>
