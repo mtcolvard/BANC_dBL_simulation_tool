b0d9ccf6db11e8e12f53ba818a58a693e8ee4b3a
mtcolvard <matthew.colvard@gmail.com>
2021-07-17 21:56:25 +0200

    transfered distantHighway

---
+++ banc_modules/audio/distantHighway.js
@@ -0,0 +1,12 @@
+<section class="distantFreeway">
+
+  <audio src="https://freesound.org/people/courter/sounds/448092/" crossorigin="anonymous" ></audio>
+
+<!-- 			type="audio/mpeg" -->
+
+  <button data-playing="false" class="tape-controls-play" role="switch" aria-checked="false">
+    <span>Play/Pause</span>
+  </button>
+</section>
+
+https://freesound.org/people/bkowshik/sounds/439218/
