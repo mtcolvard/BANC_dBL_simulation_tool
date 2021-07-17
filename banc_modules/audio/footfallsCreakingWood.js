3e3addd474b93ab0a4070fba69a48832f0aea41d
mtcolvard <matthew.colvard@gmail.com>
2021-07-17 22:02:54 +0200

    transfer footfallsCreakingWood

---
+++ banc_modules/audio/footfallsCreakingWood.js
@@ -0,0 +1,10 @@
+<section class="distantFreeway">
+
+  <audio src="https://freesound.org/people/TheBuilder15/sounds/367050/" crossorigin="anonymous" ></audio>
+
+<!-- 			type="audio/mpeg" -->
+
+  <button data-playing="false" class="tape-controls-play" role="switch" aria-checked="false">
+    <span>Play/Pause</span>
+  </button>
+</section>
